f2ab69f2e83e39e3cb5cfd1944772100d68d54e9
Thomas Broadley <buriedunderbooks@hotmail.com>
2024-09-15 12:04:27 -0700

    Get websocket working?

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,6 +1,8 @@
 import * as r from "rethinkdb";
+import WebSocket from "ws";
 import express from "express";
 import RethinkDBClient from "./rethinkdb";
+import { z } from "zod";
 
 async function initializeDatabase(rethinkDBClient: RethinkDBClient) {
   const dbList = await rethinkDBClient.executeQuery(r.dbList());
@@ -25,7 +27,6 @@ async function initializeDatabase(rethinkDBClient: RethinkDBClient) {
 
 async function main() {
   const app = express();
-  const port = process.env.PORT || 3000;
 
   const rethinkDBClient = new RethinkDBClient();
   await rethinkDBClient.connect();
@@ -36,7 +37,7 @@ async function main() {
 
   app.post("/rooms", async (req, res) => {
     try {
-      const { username } = req.body;
+      const { username } = z.object({ username: z.string() }).parse(req.body);
 
       // Create a room
       const room = await rethinkDBClient.executeQuery(
@@ -61,17 +62,36 @@ async function main() {
     }
   });
 
-  app.put("/users/:userId", async (req, res) => {
+  app.post("/rooms/:roomId/users", async (req, res) => {
     try {
-      const { userId } = req.params;
-      const { username } = req.body;
+      const { roomId } = req.params;
+      const { name } = z.object({ name: z.string() }).parse(req.body);
+
+      // Create a user associated with the room
+      const user = await rethinkDBClient.executeQuery(
+        r.db("forearm-scale").table("users").insert({ name, roomId })
+      );
+
+      res.json({
+        userId: user.generated_keys[0],
+      });
+    } catch (error) {
+      console.error("Error creating user:", error);
+      res.status(500).json({ error: "Failed to create user" });
+    }
+  });
+
+  app.put("/rooms/:roomId/users/:userId", async (req, res) => {
+    try {
+      const { roomId, userId } = req.params;
+      const { name } = z.object({ name: z.string() }).parse(req.body);
 
       await rethinkDBClient.executeQuery(
         r
           .db("forearm-scale")
           .table("users")
-          .get(userId)
-          .update({ name: username }, { returnChanges: true })
+          .filter({ id: userId, roomId })
+          .update({ name }, { returnChanges: true })
       );
     } catch (error) {
       console.error("Error updating user:", error);
@@ -79,8 +99,52 @@ async function main() {
     }
   });
 
-  app.listen(port, () => {
+  const port = process.env.PORT || 3000;
+  const server = app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
+  });
+
+  const wss = new WebSocket.Server({ noServer: true });
+  wss.on("connection", async (ws, request) => {
+    if (!request.url) {
+      ws.close(1011, "Invalid URL");
+      return;
+    }
+
+    const { pathname } = new URL(request.url, `http://${request.headers.host}`);
+    const [rooms, roomId] = pathname.split("/", 2);
+    if (rooms !== "rooms") {
+      ws.close(1011, "Invalid URL");
+      return;
+    }
+
+    const roomChanges = r
+      .db("forearm-scale")
+      .table("rooms")
+      .getAll(roomId)
+      .outerJoin(r.db("forearm-scale").table("users"), (room, user) =>
+        room("id").eq(user("roomId"))
+      )
+      .without({ left: { id: true } } as any) // TODO does this work?
+      .zip()
+      .changes();
+
+    const result = await rethinkDBClient.executeQuery(roomChanges);
+    result.each((error, change) => {
+      if (error) {
+        console.error("Error processing change:", error);
+        ws.close(1011, "Internal error");
+        return;
+      }
+
+      ws.send(JSON.stringify(change));
+    });
+  });
+
+  server.on("upgrade", (request, socket, head) => {
+    wss.handleUpgrade(request, socket, head, (ws) => {
+      wss.emit("connection", ws, request);
+    });
   });
 }
 
--- server/src/rethinkdb.ts
+++ server/src/rethinkdb.ts
@@ -1,4 +1,4 @@
-import * as r from 'rethinkdb';
+import * as r from "rethinkdb";
 
 class RethinkDBClient {
   private connection: r.Connection | null;
@@ -10,26 +10,26 @@ class RethinkDBClient {
   async connect() {
     try {
       this.connection = await r.connect({
-        host: 'localhost',
+        host: "localhost",
         port: 28015,
-        db: 'forearm-scale'
+        db: "forearm-scale",
       });
-      console.log('Connected to RethinkDB');
+      console.log("Connected to RethinkDB");
     } catch (error) {
-      console.error('Failed to connect to RethinkDB:', error);
+      console.error("Failed to connect to RethinkDB:", error);
     }
   }
 
   async executeQuery<T>(query: r.Operation<T>): Promise<T> {
     if (!this.connection) {
-      throw new Error('No connection to RethinkDB');
+      throw new Error("No connection to RethinkDB");
     }
 
     try {
       const result = await query.run(this.connection);
       return result;
     } catch (error) {
-      console.error('Failed to execute query:', error);
+      console.error("Failed to execute query:", error);
       throw error;
     }
   }
@@ -37,9 +37,9 @@ class RethinkDBClient {
   async close() {
     try {
       await this.connection?.close();
-      console.log('Connection to RethinkDB closed');
+      console.log("Connection to RethinkDB closed");
     } catch (error) {
-      console.error('Failed to close connection to RethinkDB:', error);
+      console.error("Failed to close connection to RethinkDB:", error);
     }
   }
 }
